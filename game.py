20f5d47fcacbe9cbd361dc35af336461dbb7d348
fkribs <forrestkribs@comcast.net>
2020-02-20 10:23:38 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 def play_with_bets(starting_balance=1000):
+	import math
+	import random
 	def play():
 		value = random.randint(0,9)
 		attempts = 0
@@ -23,7 +25,7 @@ def play_with_bets(starting_balance=1000):
 			print(message)
 			if guess == value or attempts == 3:
 				print("It was {}".format(value))
-				return guess == value
+				return (guess == value, attempts)
 	balance = starting_balance
 	high = balance
 	bets = []
@@ -39,16 +41,19 @@ def play_with_bets(starting_balance=1000):
 
 		if bet == '':
 			print("\n-----GAME OVER-----")
+			if rounds == 0:
+				return
 			print("Started with ${}".format(starting_balance))
 			print("Finished with ${}".format(balance))
 			knew_to_quit = "(You really knew when to quit!)" if balance == high else ""
 			print("Highest balance: ${} {}".format(high, knew_to_quit))
 			print("Win rate: {0:.2f}%".format(wins / rounds * 100))
-			print("Average bet: ${}".format(sum(bets) / len(bets)))
+			print("Average bet: ${0:.2f}".format(sum(bets) / len(bets)))
 			print("Average bet as percentage of balance: {0:.2f}%".format(sum(bets_as_percent_of_balance) / len(bets_as_percent_of_balance) * 100))
 			dare_devil = ". What a dare devil!" if all_ins == rounds else "."
-			dare_devil = dare_devil + " (Didn't work out though, did it?)" if balance < starting_balance else dare_devil
-			print("You went 'all-in' {1:.2f}% of the time ({0} time(s)){2}".format(all_ins, all_ins / rounds * 100, dare_devil))
+			dare_devil = dare_devil + " (Didn't work out though, did it?)" if balance < starting_balance and all_ins == rounds else dare_devil
+			dare_devil = " COWARD!" if all_ins == 0 else "."
+			print("You went 'all-in' {1:.2f}% of the time ({0} {3}){2}".format(all_ins, all_ins / rounds * 100, dare_devil, "time" if all_ins == 1 else "times"))
 			return
 		try:
 			bet = int(bet)
@@ -66,8 +71,17 @@ def play_with_bets(starting_balance=1000):
 		rounds += 1
 		bets.append(bet)
 		bets_as_percent_of_balance.append(bet / balance)
-		if play():
-			balance += bet
+		round_results = play()
+		if round_results[0]:
+			attempts = round_results[1]
+			bonus = 0
+			if attempts == 2:
+				bonus = math.ceil(bet * .1)
+			if attempts == 1:
+				bonus = math.ceil(bet * .5)
+			if bonus != 0:
+				print("LEFTOVER BONUS +{}!!! (You guessed correctly in {} {}!)".format(bonus, attempts, "try" if attempts == 1 else "tries"))
+			balance += (bet + bonus)
 			wins += 1
 		else:
 			balance -= bet
